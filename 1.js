57462adb4f1e4bb454f0681d76a09d713ef498a0
2359451d <2359451d@student.gla.ac.uk>
2021-12-23 23:07:51 +0000

    cleanup

---
+++ 1.js
@@ -0,0 +1,7 @@
+<div class="js-socket-channel js-updatable-content" data-channel="eyJjIjoicHJvamVjdHM6MTM5MDg2ODAiLCJ0IjoxNjQwMzAwNTI1fQ==--59824f8a2d3b2d7b8b84ee6a936d6048deaa1bb2db4e07534d40a1f9e3b27780" data-url="/_render_node/PRO_kwLOGIAY9M4A1DrI/projects/show_progress" style="width: 100px">
+  <div class="tooltipped tooltipped-s" aria-label="12 done / 5 in progress / 14 to do">
+  <span class="progress-bar progress-bar-small">
+    <span class="progress d-inline-block color-bg-success-emphasis" style="width: 38.70967741935484%">&nbsp;</span><span class="progress d-inline-block color-bg-done-emphasis" style="width: 16.129032258064516%">&nbsp;</span>
+  </span>
+</div>
+</div>
