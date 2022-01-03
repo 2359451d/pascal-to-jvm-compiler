e2101c8a7a7e79eb41163a47bdb01636218aa9f2
2359451d <2359451d@student.gla.ac.uk>
2022-01-03 23:43:13 +0000

    fix: enum resource clean up

--- src/main/java/type/TypeDescriptor.java
+++ src/main/java/type/TypeDescriptor.java
@@ -3,7 +3,7 @@ package type;
 /**
  * Base interface for BaseType, ErrorType, BuiltinType
  * <p>
- *     Usage: to uniform the return type of visitors
+ *     Usage: to uniform the return type of t visitors
  * </p>
  */
 public interface TypeDescriptor {
--- src/main/resources/pascal_test/test/enumTest.pas
+++ src/main/resources/pascal_test/test/enumTest.pas
@@ -12,10 +12,7 @@ type
 var
   card : cardsuit;
   cardsuit2 : (clubs2, diamonds2, hearts2, spades2);
-<<<<<<< HEAD
   weatherVar:weather;
-=======
->>>>>>> c0239b4f72370cd8532299c7c1c491508217c9ff
   boolVar: Boolean;
   clubs2:Integer; {duplicate identifier is not allowed}
 begin
@@ -23,21 +20,16 @@ begin
   {cardsuit2:= clubs2;}
   cardsuit2 := clubs2;
   cardsuit2 := clubs3; {Not a valid id in acceptable enum value map}
-<<<<<<< HEAD
 
   c :=1;
   cardsuit := 1; {Illegal assignment [cardsuit:=1]. Assigning value to identifier is not allowed}
   clubs2 := 1; {Illegal assignment [clubs2:=1]. Assigning value to identifieris not allowed}
 
   boolVar := clubs2 < diamonds2;
-  boolVar := true = (diamonds2 < diamonds); {Expression [diamonds2<diamonds] types are incompatible}
   boolVar := clubs < diamonds;
+  boolVar := true = (diamonds2 < diamonds); {Expression [diamonds2<diamonds] types are incompatible}
 
   boolVar := clubs2 < clubs; {Not allowed, enum kind is not the same}
   boolVar := clubs < sunny; {Not allowed, enum kind is not the same}
-=======
-  boolVar := clubs2 < diamonds2;
-  boolVar := clubs2 < clubs; {Not allowed, enum kind is not the same}
->>>>>>> c0239b4f72370cd8532299c7c1c491508217c9ff
 
 end.
