9059f231d17aa58fee9c3ce015b81c7af918c8ce
2359451d <2359451d@student.gla.ac.uk>
2022-01-23 16:35:26 +0000

    contextual-analy: packed arr, string assign done

--- src/main/resources/pascal_test/test/recordTest.pas
+++ src/main/resources/pascal_test/test/recordTest.pas
@@ -6,12 +6,12 @@ type
     pageNum:Integer;
   end;
   Books = record
-    {title: packed array [1..50] of char;
-    author: packed array [1..50] of char;
-    subject: packed array [1..100] of char;}
-    title: array [1..50] of char;
+    title: packed array [1..13] of char;
+    author: packed array [1..9] of char;
+    subject: array [1..100] of char; {unpacked char would not be recognized as string type}
+    {title: array [1..50] of char;
     author: array [1..50] of char;
-    subject: array [1..100] of char;
+    subject: array [1..100] of char;}
     Book_id: Integer;
     page: pagerecord;
     nestedArr: array [1..10, 1..10] of Integer;
@@ -31,20 +31,21 @@ begin
   (* book 1 specification *)
   Book1.title  := 'C Programming';
   Book1.author := 'Nuha Ali ';
-  Book1.subject := 'C Programming Tutorial';
+  Book1.nestedArr[1][10] := 1;
+  Book1.subject := 'C'; {unpacked character array, direct string assignment is not allowed}
+  Book1.subject[1] := 'C';
   Book1.book_id.page := 6495407; {invalid operation, cannot apply field designator on integer}
   Book1.Book_id := 6495407;
   intVar:= Book1.Book_id;
   Book1.Page.pageNum := 110;
   Book1.Page.pageNum := 110.0; {invalid type, expected int, actual real}
-  Book1.nestedArr[1][10] := 1;
 
   printRecord(Book1);
 
   (* book 2 specification *)
-  Book2.title := 'Telecom Billing';
-  Book2.author := 'Zara Ali';
-  Book2.subject := 'Telecom Billing Tutorial';
+  Book2.title := 'Telecom Billing'; {length cannot match}
+  Book2.author := 'Zara Ali ';
+  Book2.subject := 'Telecom Billing Tutorial'; {unpacked arr}
   Book2.book_id := 6495700;
 
   (* print Book1 info *)
