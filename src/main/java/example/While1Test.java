f5d475b72da9c5cade37fe1c2505dac67e83d033
Yao Du <46606028+2359451d@users.noreply.github.com>
2022-02-22 10:12:13 +0000

    #112 while (#121)
    * feat(code-gen): if-state done
    
    * fix: constant if fixed
    
    * feat(code-gen): while statement

---
+++ src/main/java/example/While1Test.java
@@ -0,0 +1,13 @@
+package example;
+
+public class While1Test {
+    private static int a;
+
+    public static void main(String[] args) {
+        a = 10;
+        while (a < 20) {
+            //System.out.println("value of a: "+a);
+            a = a + 1;
+        }
+    }
+}
--- src/test/resources/driver/testPascalRun/if1Test.pas
+++ src/test/resources/driver/testPascalRun/if1Test.pas
@@ -7,7 +7,7 @@
 program if1Test;
 var intvar:Integer;
 begin
-  intvar := 9;
+  {intvar := 9;}
   if intvar>= 10 then
     WriteLn('greater than 10')
   else writeln('intvar <10');
