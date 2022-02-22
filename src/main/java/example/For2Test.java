85ea947e257158b6dd76e4734626dff762c743f1
2359451d <2359451d@student.gla.ac.uk>
2022-02-22 11:15:32 +0000

    feat(code-gen): for statement

---
+++ src/main/java/example/For2Test.java
@@ -0,0 +1,11 @@
+package example;
+
+public class For2Test {
+    private static int a;
+
+    public static void main(String[] args) {
+        for (a = 10; a <= 20; a++) {
+            System.out.println("value of a: "+a);
+        }
+    }
+}
